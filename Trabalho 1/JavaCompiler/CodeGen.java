ad3bcb9cb72c63e105e352e58ab5e86262293808
Marlon Dias <marlonsdias@gmail.com>
2014-10-01 21:58:59 -0300

    Compilador com geração de código e interpretador implementado

--- Trabalho 1/JavaCompiler/CodeGen.java
+++ Trabalho 1/JavaCompiler/CodeGen.java
@@ -18,9 +18,6 @@ class CodeGen{
 			geraCodigo2(((Soma) arv).arg2) +
 			"SUM\n");
 
-	if (arv instanceof Num)
-		return ("PUSH "  + ((Num) arv).num + "\n");
-
 	if (arv instanceof Sub)
 		return (geraCodigo2(((Sub) arv).arg1) + 
 			geraCodigo2(((Sub) arv).arg2) +
@@ -31,6 +28,47 @@ class CodeGen{
 			geraCodigo2(((Div) arv).arg2) +
 			"DIV\n");
 
+	if (arv instanceof Num)
+		return ("PUSH "  + ((Num) arv).num + "\n");
+
 	return "";
+	}
+
+	String interpretador(ArvoreSintatica arv) throws Exception{
+		try{
+			return ("Resultado da expressão é: "+Float.toString(interpretador2(arv)));
+		} catch (Exception e){
+			throw e;
+		}
+	}
+
+	float interpretador2(ArvoreSintatica arv) throws Exception {
+
+
+		if (arv instanceof Mult)
+			return (interpretador2(((Mult) arv).arg1) * 
+			interpretador2(((Mult) arv).arg2));
+
+		if (arv instanceof Soma)
+			return (interpretador2(((Soma) arv).arg1) +
+			interpretador2(((Soma) arv).arg2));
+
+		if (arv instanceof Div){
+
+			if (interpretador2(((Div) arv).arg2) == 0)
+				throw (new Exception("Não é possível efetura divisão por zero."));
+
+			return (interpretador2(((Div) arv).arg1) / 
+			interpretador2(((Div) arv).arg2));
+		}
+
+		if (arv instanceof Sub)
+			return (interpretador2(((Sub) arv).arg1) -
+			interpretador2(((Sub) arv).arg2));
+
+		if (arv instanceof Num)
+			return (((Num) arv).num);
+
+		return 0;
 	}
 }
--- Trabalho 1/JavaCompiler/Compilador.java
+++ Trabalho 1/JavaCompiler/Compilador.java
@@ -8,13 +8,35 @@ class Compilador{
 
 			AnaliseLexica al = new AnaliseLexica(args[0]);
 			Parser as = new Parser(al);
-		
+			int op = 0;
+
+			if (args.length > 1){
+				if ("codegen".equals(args[1].toLowerCase()))
+					op = 0;
+				else
+					if ("interpretador".equals(args[1].toLowerCase()))
+						op = 1;
+					else{
+						System.err.println(args[1] + " é um parâmetro inválido.");
+        				System.exit(1);
+        			}
+			}
+
 			arv = as.parseProg();
-		
-			
+
 			CodeGen backend = new CodeGen();
-			String codigo = backend.geraCodigo(arv);
-			System.out.println(codigo);
+
+			if (op == 1){
+				try{
+					String out = backend.interpretador(arv);
+					System.out.println(out);
+				} catch (Exception e){
+					System.out.println(e);
+				}
+			} else {
+				String codigo = backend.geraCodigo(arv);
+				System.out.println(codigo);
+			}
 
 		}catch(Exception e)
 		{			
